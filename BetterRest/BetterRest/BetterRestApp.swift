2621951c0c7620d2f8bbf52fa8ed5ca79e5d03d7
devans1 <devans1@users.noreply.github.com>
2022-04-14 06:04:24 +1000

    Starting BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by David Evans on 14/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
