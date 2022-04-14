58bf1ad8eb46d3cdf1f6bef11cfc0cfc204136c9
devans1 <devans1@users.noreply.github.com>
2022-04-14 14:24:19 +1000

    Initial Project 6

---
+++ Project6/Project6/Project6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6App.swift
+//  Project6
+//
+//  Created by David Evans on 14/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
