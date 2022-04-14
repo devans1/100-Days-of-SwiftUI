143c668b50d120599b33d55bea60e0bc202b778f
devans1 <devans1@users.noreply.github.com>
2022-04-14 15:55:19 +1000

    Multiple animations

--- Project6/Project6/ContentView.swift
+++ Project6/Project6/ContentView.swift
@@ -8,24 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         print("\(animationAmount)") ; return
         VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(.easeInOut(duration: 1)
-                .repeatCount(3, autoreverses: true)), in: 1...10)
+            // How cool, it seems to call the rotation3DEffect from here too!
+            Stepper("Scale amount", value: $animationAmount.animation(.interpolatingSpring(stiffness: 10, damping: 1)), step: 360)
             
             Spacer()
             
             Button("Tap Me") {
-                animationAmount += 1
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    animationAmount += 360
+                }
             }
             .padding(40)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .scaleEffect(animationAmount)
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
+            Spacer()
+
+            Button("Tap Me Too") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(nil, value: enabled)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+            Spacer()
+
         }
     }
 }
