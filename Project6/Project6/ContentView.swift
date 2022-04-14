9b040f1053be957d1fd520ecd1e16da5d3c9a6f4
devans1 <devans1@users.noreply.github.com>
2022-04-14 15:08:29 +1000

    Some nice animations

--- Project6/Project6/ContentView.swift
+++ Project6/Project6/ContentView.swift
@@ -11,16 +11,34 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
+        VStack {
+            // this was no use it did not show any change :(
+            // but worked out there is no auto increment happening, it just moves between two values 1 and 0
+            Text("animationAmount=\(animationAmount)")
+                .padding(80)
+
+            Button("Tap Me") {
+    //            animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
     }
 }
 
