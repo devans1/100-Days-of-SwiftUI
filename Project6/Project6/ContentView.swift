4ba7d1d9b0d183b7fe9aab9cb75a266232d23b81
devans1 <devans1@users.noreply.github.com>
2022-04-14 15:19:54 +1000

    Explicit animations

--- Project6/Project6/ContentView.swift
+++ Project6/Project6/ContentView.swift
@@ -11,33 +11,21 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
+        print("\(animationAmount)") ; return
         VStack {
-            // this was no use it did not show any change :(
-            // but worked out there is no auto increment happening, it just moves between two values 1 and 0
-            Text("animationAmount=\(animationAmount)")
-                .padding(80)
-
+            Stepper("Scale amount", value: $animationAmount.animation(.easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)), in: 1...10)
+            
+            Spacer()
+            
             Button("Tap Me") {
-    //            animationAmount += 1
+                animationAmount += 1
             }
-            .padding(50)
+            .padding(40)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeOut(duration: 1)
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
-            }
+            .scaleEffect(animationAmount)
         }
     }
 }
