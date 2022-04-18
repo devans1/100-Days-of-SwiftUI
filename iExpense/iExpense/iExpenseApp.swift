703e48bae1eabc469424f97ea45f33e939b84aa7
devans1 <devans1@users.noreply.github.com>
2022-04-19 06:31:02 +1000

    Start of iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by David Evans on 19/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
