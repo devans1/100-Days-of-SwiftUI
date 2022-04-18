fcbf2407dd51834fe15e88cb693e097a9522dd17
devans1 <devans1@users.noreply.github.com>
2022-04-19 08:58:07 +1000

    Exercises, before main iExpenses

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -12,18 +12,67 @@ class User: ObservableObject {
     @Published var lastName = "Baggins"
 }
 
+struct SecondView: View {
+    let name: String
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack {
+            Text("Hello, \(name)!")
+            Button(action: {
+                dismiss()
+            }, label: {
+                Text("Dismiss?")
+            })
+        }
+    }
+}
 
 struct ContentView: View {
     @StateObject private var user = User()
+    @State private var showingSheet = false
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
 
     var body: some View {
         print("\(user.firstName) \(user.lastName)") ; return
         VStack {
+            Spacer()
+            Button(action: {
+                showingSheet.toggle()
+            }, label: {
+                Text("Show sheet")
+            })
+            
+            Spacer()
             Text("Your name is \(user.firstName) \(user.lastName).")
 
             TextField("First name", text: $user.firstName)
             TextField("Last name", text: $user.lastName)
+            Spacer()
+
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
+            Spacer()
         }
+        .sheet(isPresented: $showingSheet) {
+            // contents of the sheet
+            SecondView(name: "@TwoStraws")
+        }
+    }
+
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
