b6fc69eb3b1ea7ec796059a0d8eaae6cdeca25ff
devans1 <devans1@users.noreply.github.com>
2022-04-20 06:41:54 +1000

    ScrollView and LazyVStack

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,14 +7,39 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText \(text)")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 10) {
+                GeometryReader { geo in
+                    Image("Example")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width * 0.8)
+                        .frame(width: geo.size.width, height: geo.size.height)
+                }
+                .frame(width: 100, height: 100)
+
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+                .frame(maxWidth: .infinity)
+                
+            }
         }
     }
 }
