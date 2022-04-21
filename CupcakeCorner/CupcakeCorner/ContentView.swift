4226314d6e09442e3e37274fc101e6c00a4bd615
devans1 <devans1@users.noreply.github.com>
2022-04-22 08:08:17 +1000

    Disabling a form

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -41,7 +41,42 @@ struct ContentView: View {
     
     @State private var results = [Result]()
     
+    @State private var username = ""
+    @State private var email = ""
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+
+    
     var body: some View {
+    
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+            
+        }
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
         
         List(results, id: \.trackId) { item in
             VStack(alignment: .leading) {
@@ -64,7 +99,7 @@ struct ContentView: View {
         }
         
         do {
-            let (data, _) = try await URLSession.shared.data(from: url) //
+            let (data, _) = try await URLSession.shared.data(from: url)
 
             if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
                 results = decodedResponse.results
