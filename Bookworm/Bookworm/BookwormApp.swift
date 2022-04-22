3c8a3176b8e57031bc22983055e1cb01c062fae6
devans1 <devans1@users.noreply.github.com>
2022-04-22 13:50:00 +1000

    Initial Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by David Evans on 22/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
