82d3f1c51d8a3bdbda14c31c7bb2c5a3a196845b
devans1 <devans1@users.noreply.github.com>
2022-04-22 14:59:20 +1000

    Pushbutton and Textfield

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,10 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    // Note that AppStorage and UserDefaults is not private so don't use if for any PII!!!
+    @AppStorage("notes") private var notes = ""
+
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            PushButton(title: "PushButton", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+            
+            Spacer()
+            
+            NavigationView {
+                TextEditor(text: $notes)
+                    .navigationTitle("Notes")
+                    .padding()
+            }
+            
+            Spacer()
+            
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }
+        }
     }
 }
 
---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,23 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by David Evans on 22/4/2022.
+//
+
+import Foundation
+import CoreData
+
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
