049f9126aee60de6f640ccf76d429c975d8f2186
devans1 <devans1@users.noreply.github.com>
2022-04-26 09:43:02 +1000

    Bookworm done

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,9 +10,69 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+//        SortDescriptor(\.title, order: .reverse),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
 
     var body: some View {
-        Text("hello world")
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                    .foregroundColor( book.rating > 1 ? .primary : .red)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add Book", systemImage: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
+            
+        }
+        
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+
+            // delete it from the context
+            moc.delete(book)
+        }
+
+        // save the context
+//        try? moc.save()
     }
 }
 
---
+++ CoreDataTest/CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by David Evans on 25/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
