41b0357961a160d6b1a4b08a722a0c67a3ba1322
devans1 <devans1@users.noreply.github.com>
2022-04-27 15:10:13 +1000

    Start of Friends

---
+++ Friends/Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by David Evans on 27/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Friends/Friends/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Friends
+//
+//  Created by David Evans on 27/4/2022.
+//
+
+import Foundation
