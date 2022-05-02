b445767447d145c97d5694bcb463ac52faddebca
devans1 <devans1@users.noreply.github.com>
2022-05-03 08:42:38 +1000

    Changes to fix MapKit - ignore last commit for MapKit

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,10 +8,6 @@
 import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
 struct Location: Identifiable {
     let id = UUID()
     let name: String
@@ -19,92 +15,40 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
-    struct LoadingView: View {
-        var body: some View {
-            Text("Loading...")
-        }
-    }
-
-    struct SuccessView: View {
-        var body: some View {
-            Text("Success!")
-        }
-    }
-
-    struct FailedView: View {
-        var body: some View {
-            Text("Failed.")
-        }
-    }
-
+    
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
                                                       span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
 
     var body: some View {
-        var loadingState: LoadingState = .loading
-        let locations = [
-            Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-            Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-        ]
         
-        VStack {
-            NavigationView {
-                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-    //                MapMarker(coordinate: location.coordinate)
-                    MapAnnotation(coordinate: location.coordinate) {
-//                        VStack {
-                            NavigationLink {
-                                Text(location.name)
-                            } label: {
-                                Circle()
-                                    .stroke(.red, lineWidth: 3)
-                                    .frame(width: 44, height: 44)
-    //                                .onTapGesture {
-    //                                    print("Tapped on \(location.name)")
-    //                                }
-    //                            Text(location.name)
-                            }
-//                        }
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                //                MapMarker(coordinate: location.coordinate)
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        VStack {
+                            Circle()
+                                .stroke(.red, lineWidth: 3)
+                                .frame(width: 44, height: 44)
+//                                .onTapGesture {
+//                                    print("Tapped on \(location.name)")
+//                                }
+                            Text(location.name)
+    
+                        }
                     }
                 }
             }
-            navigationTitle("London Explorer")
-            Text("Hello World")
-                .onTapGesture {
-                    let str = "Test Message"
-                    let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                    
-                    do {
-                        try str.write(to: url, atomically: true, encoding: .utf8)
-                        let input = try String(contentsOf: url)
-                        print(input)
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-            Group {
-                switch loadingState {
-                case .loading:
-                    LoadingView()
-                case .success:
-                    SuccessView()
-                case .failed:
-                    FailedView()
-                }
-            }
-
         }
-        
+        .navigationTitle("London Explorer")
     }
-    
-    func getDocumentsDirectory() -> URL {
-        // find all possible documents directories for this user
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
-        // just send back the first one, which ought to be the only one
-        return paths[0]
-    }
-    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
