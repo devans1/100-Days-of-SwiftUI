3071b99cc1b15cca0368662e03c2df9e8ebb61b9
devans1 <devans1@users.noreply.github.com>
2022-05-03 09:29:41 +1000

    Local authentication

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,50 +5,54 @@
 //  Created by David Evans on 2/5/2022.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
 
 struct ContentView: View {
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
-                                                      span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
 
     var body: some View {
-        
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                //                MapMarker(coordinate: location.coordinate)
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        VStack {
-                            Circle()
-                                .stroke(.red, lineWidth: 3)
-                                .frame(width: 44, height: 44)
-//                                .onTapGesture {
-//                                    print("Tapped on \(location.name)")
-//                                }
-                            Text(location.name)
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+
+            } else {
+                Text("Locked")
+
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
     
-                        }
-                    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data." // only used for touch ID, Face ID is in info.plist
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
                 }
             }
-            .navigationTitle("London Explorer")
+        } else {
+            // no biometrics
         }
     }
-
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
