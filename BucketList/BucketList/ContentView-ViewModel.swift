e7416e122bbc6fd140c657e8cb3f9261ca0e4349
devans1 <devans1@users.noreply.github.com>
2022-05-03 15:35:49 +1000

    Bucket List done but before Challenges

--- BucketList/BucketList/ContentView-ViewModel.swift
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -5,10 +5,85 @@
 //  Created by David Evans on 3/5/2022.
 //
 
-import MapKit
 import Foundation
+import MapKit
+import LocalAuthentication
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {   // MainActor runs all UI stuff, all this code will also run on the MainActor unless we explicitly tell it otherwise
+        
+        @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
+                                                      span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        //        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+                
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+        
+        func addLocation() {
+            let newLocation = Location(id: UUID(),
+                                       name: "New location",
+                                       description: "",
+                                       latitude: mapRegion.center.latitude,
+                                       longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    
+                    if success {
+//                        Task {  // create a background task and then wait for MainActor to run this closure - not the best as jumping between threads
+//                            await MainActor.run {
+//                                self.isUnlocked = true
+//                            }
+//                        }
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+        }
+        
     }
 }
--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -12,97 +12,70 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State private var isUnlocked = false
-    
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
-                                                      span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-    @State private var locations = [Location]()
-    @State private var selectedPlace: Location?
+    @StateObject private var viewModel = ViewModel()
 
     var body: some View {
-        
-        ZStack {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                //                MapMarker(coordinate: location.coordinate)
-                MapAnnotation(coordinate: location.coordinate) {
-                    VStack {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundColor(.red)
-                            .frame(width: 44, height: 44)   // minimum size recommended by Apple UI guidelines
-                            .background(.white)
-                            .clipShape(Circle())
-                        
-                        Text(location.name)
-                            .fixedSize()    // to get around what is probably a bug in Swift UI where it clips name till you move map
-                    }
-                    .onTapGesture {
-                        selectedPlace = location
+        if viewModel.isUnlocked {
+            ZStack {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    //                MapMarker(coordinate: location.coordinate)
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)   // minimum size recommended by Apple UI guidelines
+                                .background(.white)
+                                .clipShape(Circle())
+                            
+                            Text(location.name)
+                                .fixedSize()    // to get around what is probably a bug in Swift UI where it clips name till you move map
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
                     }
                 }
-            }
-            .ignoresSafeArea()
-            Circle()
-                .fill(.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            VStack {
-                Spacer()
-                HStack {
+                .ignoresSafeArea()
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                VStack {
                     Spacer()
-                    Button {
-                        // create a new location
-                        let newLocation = Location(id: UUID(),
-                                                   name: "New location",
-                                                   description: "",
-                                                   latitude: mapRegion.center.latitude,
-                                                   longitude: mapRegion.center.longitude)
-                        locations.append(newLocation)
-                    } label: {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        Button {
+                            // create a new location
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
             }
-        }
-        .sheet(item: $selectedPlace) { place in
-            EditView(location: place) { newLocation in
-                if let index = locations.firstIndex(of: place) {
-                    locations[index] = newLocation
+            .sheet(item: $viewModel.selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    viewModel.update(location: newLocation)
                 }
             }
+        } else {
+            // button here to unlock
+            Button("Unlock Places") {
+                viewModel.authenticate()
+            }
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
         }
-
     }
-    
-    
-//    func authenticate() {
-//        let context = LAContext()
-//        var error: NSError?
-//
-//        // check whether biometric authentication is possible
-//        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-//            // it's possible, so go ahead and use it
-//            let reason = "We need to unlock your data." // only used for touch ID, Face ID is in info.plist
-//
-//            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-//                // authentication has now completed
-//                if success {
-//                    // authenticated successfully
-//                    isUnlocked = true
-//                } else {
-//                    // there was a problem
-//                }
-//            }
-//        } else {
-//            // no biometrics
-//        }
-//    }
         
 }
 
