46e949a28a2ae2f8f86487382ffb982ed288ad28
devans1 <devans1@users.noreply.github.com>
2022-05-05 05:59:27 +1000

    Start of accessability

---
+++ Accessability/Accessability/AccessabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilityApp.swift
+//  Accessability
+//
+//  Created by David Evans on 5/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
