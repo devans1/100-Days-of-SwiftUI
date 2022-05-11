b793658946ba49ccc7fdb48a1ada69f2dbf26d5c
devans1 <devans1@users.noreply.github.com>
2022-05-12 06:35:04 +1000

    Template

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by David Evans on 12/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
