b615c0931dcff05631da9fee3bcd4bb3d758a0f0
devans1 <devans1@users.noreply.github.com>
2022-05-12 14:55:59 +1000

    Blank HotProspects1

---
+++ HotProspects1/HotProspects1/HotProspects1App.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspects1App.swift
+//  HotProspects1
+//
+//  Created by David Evans on 12/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspects1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
