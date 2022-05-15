c39e927224f678ddd61605d3602f4b5101f55ea4
devans1 <devans1@users.noreply.github.com>
2022-05-16 09:07:19 +1000

    Starting Flashzilla

---
+++ Flashzilla-prep/Flashzilla-prep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla-prep
+//
+//  Created by David Evans on 16/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashzilla-prep/Flashzilla-prep/Flashzilla_prepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashzilla_prepApp.swift
+//  Flashzilla-prep
+//
+//  Created by David Evans on 16/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct Flashzilla_prepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
