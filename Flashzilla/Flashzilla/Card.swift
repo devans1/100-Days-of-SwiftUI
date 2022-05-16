9b117ebbe572596aedc2fca7854db4f4b03f1e6b
devans1 <devans1@users.noreply.github.com>
2022-05-17 09:56:38 +1000

    Flashzilla done but before challenges

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by David Evans on 16/5/2022.
+//
+
+import Foundation
+
+struct Card: Codable {
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?",
+                              answer: "Jodie Whittaker")
+}
